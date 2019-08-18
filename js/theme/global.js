ecc9274aec1f25118b0467c95cf6531fe48e7a8c
Michael McNamara <michael@lightopiaonline.com>
2019-08-18 16:26:28 -0700

    merged previous files and new them together for history. Altered vav-pages to change breakpoint on menu to small. Menu was not showing on ipad or desktop when browser window was smaller than full width.

---
+++ js/theme/global.js
@@ -0,0 +1,56 @@
+import $ from 'jquery';
+import './common/select-option-plugin';
+import 'html5-history-api';
+import PageManager from './page-manager';
+import quickSearch from './global/quick-search';
+import currencySelector from './global/currency-selector';
+import mobileMenuToggle from './global/mobile-menu-toggle';
+import menu from './global/menu';
+import foundation from './global/foundation';
+import quickView from './global/quick-view';
+import cartPreview from './global/cart-preview';
+import compareProducts from './global/compare-products';
+import privacyCookieNotification from './global/cookieNotification';
+import maintenanceMode from './global/maintenanceMode';
+import carousel from './common/carousel';
+import 'lazysizes';
+import loadingProgressBar from './global/loading-progress-bar';
+import FastClick from 'fastclick';
+import sweetAlert from './global/sweet-alert';
+
+function fastClick(element) {
+    return new FastClick(element);
+}
+
+function maybeMenu() {
+    window.addEventListener('resize', function(){
+        if(window.innerWidth >= 568){
+            menu();
+        }
+    })
+}
+
+export default class Global extends PageManager {
+    /**
+     * You can wrap the execution in this method with an asynchronous function map using the async library
+     * if your global modules need async callback handling.
+     * @param next
+     */
+    loaded(next) {
+        fastClick(document.body);
+        quickSearch();
+        currencySelector();
+        foundation($(document));
+        quickView(this.context);
+        cartPreview();
+        compareProducts(this.context.urls);
+        carousel();
+        mobileMenuToggle();
+        privacyCookieNotification();
+        maintenanceMode(this.context.maintenanceMode);
+        loadingProgressBar();
+        sweetAlert();
+        maybeMenu();
+        next();
+    }
+}
